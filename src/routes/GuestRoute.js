4f0a991700a9f9863f8c1d4412f48b5d110501e4
Lucas Costa <lucascostacruzsilva@gmail.com>
2019-12-14 02:43:29 -0300

    Remove gh-pages

--- src/routes/GuestRoute.js
+++ src/routes/GuestRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import { Route, Redirect } from "react-router-dom";
+import { HashRouter as Route, Redirect } from "react-router-dom";
 export const GuestRoute = ({component: Component, token, ...rest}) => {
   return (
     <Route
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from "redux-persist/integration/react";
 import {persistor, store} from '../store';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, HashRouter as Route, Switch } from "react-router-dom";
 import GuestRoute from './GuestRoute';
 import PrivateRoute from "./PrivateRoute";
 
